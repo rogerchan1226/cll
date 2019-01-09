2278479dff9627453dd64fd5eff558bb541579be
Roger Chan <roger_chan@calcomp.com.tw>
2019-01-09 19:17:50 +0800

    hp_hw_wifiset_05

--- hw_01_wifiset.c
+++ hw_01_wifiset.c
@@ -2,8 +2,7 @@
 #include <stdlib.h>
 
 
-int wpa_none()
-{
+int wpa_none(){
 	char SSID [256] = "/home/rtl/wpa_cli -iwlan0 set_network 0 ssid '\"%s\"'";
 
 	system("/home/rtl/wpa_cli -iwlan0 remove_network 0");
@@ -11,6 +10,18 @@ int wpa_none()
 	system("/home/rtl/wpa_cli -iwlan0 set_network 0 key_mgmt NONE");
 	system("/home/rtl/wpa_cli -iwlan0 enable_network 0");
 	system("/home/rtl/wpa_cli -iwlan0 save_config");
+	return 0;
+}
+
+int wpa_wep(){
+	return 0;
+}
+
+int wpa_wpa(){
+	return 0;
+}
+
+int wpa_mix(){
 	return 0;
 }
 
@@ -22,15 +33,26 @@ int main(){
 	printf("Select your Wifi Security Type: \n 1.None Mode\n 2.WEP\n 3.WPA\n 4.MIX\n");
 	scanf("%d", &Stype);
 
-	if(Stype == 1)
+	if(Stype == 1){
+
 		printf("Write down SSID:\n");
 		scanf("%s", &SSID);
 		wpa_none();
 
-	//else
-	//	return 0;
+	}else if{
 
+		wpa_wep();
 
-	exit (0);
+	}else if{
+
+		wpa_wpa();
+
+	}else if{
+	
+		wpa_mix();
+	
+	}
+
+	return 0;
 
 }
