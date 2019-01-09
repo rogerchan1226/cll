523530070d4542c4c552580054a63f4160320d58
Roger Chan <roger_chan@calcomp.com.tw>
2019-01-09 15:59:14 +0800

    hp_hw_01

---
+++ hw_01_wifiset.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(){
+
+	int Stype;
+
+	printf("Select your Wifi Security Type: \n 1.None Mode\n 2.WEP\n 3.WPA\n 4.MIX\n");
+	scanf("%d", &Stype);
+
+	if(Stype == 1)
+
+
+	exit (0);
+
+}
