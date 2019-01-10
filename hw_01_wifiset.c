3d3ce293aee243ced3c7375bd19c50c4b2f65422
Roger Chan <roger_chan@calcomp.com.tw>
2019-01-10 11:10:39 +0800

    hp_hw_wifiset_06

--- hw_01_wifiset.c
+++ hw_01_wifiset.c
@@ -3,13 +3,25 @@
 
 
 int wpa_none(){
-	char SSID [256] = "/home/rtl/wpa_cli -iwlan0 set_network 0 ssid '\"%s\"'";
 
+	char ssid_set[256] = "/home/rtl/wpa_cli -iwlan0 set_network 0 ssid '\"%s\"'";
+	char SSID[256] = {0};
+	char system_cmd[256];
+	//char SSID [256] = "/home/rtl/wpa_cli -iwlan0 set_network 0 ssid '\"%s\"'";
+
+	printf("Write down SSID:\n");
+	scanf("%s", SSID);
+	sprintf(system_cmd, ssid_set, SSID);
+
+	
+	//system("/home/rtl/wpa_cli -iwlan0 set_network 0 ssid '"SSID"'");
 	system("/home/rtl/wpa_cli -iwlan0 remove_network 0");
 	system("/home/rtl/wpa_cli -iwlan0 add_network 0");
 	system("/home/rtl/wpa_cli -iwlan0 set_network 0 key_mgmt NONE");
+	system(system_cmd);
 	system("/home/rtl/wpa_cli -iwlan0 enable_network 0");
 	system("/home/rtl/wpa_cli -iwlan0 save_config");
+	
 	return 0;
 }
 
@@ -25,33 +37,35 @@ int wpa_mix(){
 	return 0;
 }
 
-int main(){
+int main(void){
 
 	int Stype;
-	int SSID;
 
-	printf("Select your Wifi Security Type: \n 1.None Mode\n 2.WEP\n 3.WPA\n 4.MIX\n");
+	printf("Select your Wifi Security Type: \n 1.None Mode\n 2.WEP Mode\n 3.WPA Mode\n 4.MIX Mode\n");
 	scanf("%d", &Stype);
 
 	if(Stype == 1){
 
-		printf("Write down SSID:\n");
-		scanf("%s", &SSID);
 		wpa_none();
 
-	}else if{
+	}else if(Stype == 2){
 
 		wpa_wep();
 
-	}else if{
+	}else if(Stype == 3){
 
 		wpa_wpa();
 
-	}else if{
+	}else if(Stype == 4){
 	
 		wpa_mix();
 	
+	}else if(Stype >= 5 || Stype <= 0){
+	
+		printf("Error!!\n");
+	
 	}
+
 
 	return 0;
 
