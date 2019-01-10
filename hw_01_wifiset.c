e4ce73bc1aae00dd9ca8100c4c9095c2b92cd410
Roger Chan <roger_chan@calcomp.com.tw>
2019-01-10 11:29:00 +0800

    hp_hw_wifiset_07

--- hw_01_wifiset.c
+++ hw_01_wifiset.c
@@ -7,14 +7,15 @@ int wpa_none(){
 	char ssid_set[256] = "/home/rtl/wpa_cli -iwlan0 set_network 0 ssid '\"%s\"'";
 	char SSID[256] = {0};
 	char system_cmd[256];
-	//char SSID [256] = "/home/rtl/wpa_cli -iwlan0 set_network 0 ssid '\"%s\"'";
+	/*char SSID [256] = "/home/rtl/wpa_cli -iwlan0 set_network 0 ssid '\"%s\"'";
+	  The mistake I made*/
 
 	printf("Write down SSID:\n");
 	scanf("%s", SSID);
 	sprintf(system_cmd, ssid_set, SSID);
 
-	
-	//system("/home/rtl/wpa_cli -iwlan0 set_network 0 ssid '"SSID"'");
+	/*system("/home/rtl/wpa_cli -iwlan0 set_network 0 ssid '"SSID"'");
+	  The mistake I made*/
 	system("/home/rtl/wpa_cli -iwlan0 remove_network 0");
 	system("/home/rtl/wpa_cli -iwlan0 add_network 0");
 	system("/home/rtl/wpa_cli -iwlan0 set_network 0 key_mgmt NONE");
