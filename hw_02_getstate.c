de0bd5b91ef83fe425d2846f126af15153b5f166
Roger Chan <roger_chan@calcomp.com.tw>
2019-01-15 15:07:44 +0800

    hp_hw_getstate_02

--- hw_02_getstate.c
+++ hw_02_getstate.c
@@ -4,8 +4,6 @@
 
 
 
-
-
 int main(){
 	
 	//char *p;
@@ -13,16 +11,24 @@ int main(){
 	FILE *fpr;
 	//int i;
 	char wifi_status[256]={0};
+	//char state_string[256]="wpa_state=";
 	//char strbuff[25]={0};
 	
 	system("/home/rtl/wpa_cli -iwlan0 status > /tmp/status.txt");
 	fpr=fopen("/tmp/status.txt", "r");
 	while(fgets(wifi_status, 256, fpr) != NULL){
+		int state_buffer = strncmp(wifi_status, "wpa_state=", 10);
 	//p = strstr(strbuff, "wpa_state=");
 	//p += strlen("wpa_state=");	
 	//q=strstr(p,"\n");
 	//strncpy(wifi_status, p, q-p);
-		printf("%s", wifi_status);
+		if(state_buffer == 0){
+			printf("%s", wifi_status);
+			break;
+			
+		}else {
+			continue;
+		}
 	}
 	fclose(fpr);
 
