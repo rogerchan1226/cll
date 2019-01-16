308a8937d5699fe6fbeb64d33a6d515558ea6b1d
Roger Chan <roger_chan@calcomp.com.tw>
2019-01-16 09:04:53 +0800

    hp_hw_wpstate_02

--- hw_03_wpstate.c
+++ hw_03_wpstate.c
@@ -1,24 +1,39 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 
 
+int timer(){
+	int countdown = 30;
+
+	while(countdown != 0){
+		printf("%d\n", countdown);
+		countdown --;
+
+	}
+	return 0;
+}
 
 int wps_pbc(){
+	int v;
+
 	system("/home/rtl/wpa_cli -iwlan0 wps_pbc");
 	system("/home/rtl/wpa_cli -iwlan0 status > /tmp/status.txt");
-	
+
+	v= timer();
+	return 0;
 }
 
 int wps_pin(){
-
+	return 0;
 }
 
 int main(){
 	int wps_t;
 
-	for{
-		printf("Select wps type:\n 1.pbc\n 2.pin");
-		scanf("%d", wps_t);
+	while(1){
+		printf("Select wps type:\n 1.pbc\n 2.pin\n");
+		scanf("%d", &wps_t);
 		if(wps_t ==1){
 			wps_pbc();
 			break;
