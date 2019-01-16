5f2ceb9d45f43faa83ff0b4901f388b130654914
Roger Chan <roger_chan@calcomp.com.tw>
2019-01-16 20:24:12 +0800

    hp_hw_wpstate_06

--- hw_03_wpstate.c
+++ hw_03_wpstate.c
@@ -4,6 +4,54 @@
 #include <string.h>
 
 
+int get_state(){
+	unsigned char *ssid;
+	unsigned char *wpa;
+	unsigned char *psk;
+	FILE *fpr=NULL;
+	unsigned char wifi_ssid[256]={0};
+	unsigned char wifi_wpa[256]={0};
+	unsigned char wifi_psk[256]={0};
+
+	fpr=fopen("/tmp/status.txt", "r");
+	while(fgets(wifi_ssid, 256, fpr) != NULL && fgets(wifi_wpa, 256, fpr) != NULL){
+
+		unsigned int state_ssid_buffer = strncmp(wifi_ssid, "ssid=", 5);	//***
+		unsigned int state_wpa_buffer = strncmp(wifi_wpa, "key_mgmt=", 8);
+		if(state_ssid_buffer == 0){
+			ssid = strstr(wifi_ssid, "ssid=");		//strstr = search string
+			ssid += strlen("ssid=");
+			printf("SSID: %s", ssid);
+			//fclose(fpr);	
+			continue;
+			
+		}else if(state_wpa_buffer == 0){
+			wpa = strstr(wifi_wpa, "key_mgmt=");		//strstr = search string
+			wpa += strlen("key_mgmt=");
+			printf("WPA: %s", wpa);
+			fclose(fpr);
+			break;
+		}
+		continue;
+			
+	}
+
+	fpr=fopen("/settings/wifi/wpa_sta.conf", "r");				//!!!!!!!!!!!!!!! bug here !!!!!!!!!!!! first line == NULL
+	while(fgets(wifi_psk, 256, fpr) != NULL){
+		unsigned int state_psk_buffer = strncmp(wifi_psk, "        psk=", 11);
+		if(state_psk_buffer == 0){
+			psk = strstr(wifi_psk, "psk=");		//strstr = search string
+			psk += strlen("psk=");
+			printf("PSK: %s", psk);
+			fclose(fpr);	
+			break;
+				
+		}
+		continue;
+	}
+
+	return 0;
+}
 
 int delay(unsigned int secs) {
 	int retTime = time(0) + secs;   
@@ -12,7 +60,6 @@ int delay(unsigned int secs) {
 
 int timer(wps_t){
 	unsigned char *front;
-	unsigned char *pin;
 	FILE *fpr=NULL;
 	unsigned int start_t;
 	unsigned long tm_sec = 180;
@@ -28,6 +75,7 @@ int timer(wps_t){
 			fgets(wifi_pin, 256, fpr);
 			printf("%s\n", wifi_pin);
 			fclose(fpr);
+
 		}
 		//printf("%s:%d\n",__func__,__LINE__);
 		system("/home/rtl/wpa_cli -iwlan0 status > /tmp/status.txt");
@@ -77,34 +125,40 @@ int timer(wps_t){
 	return 0;
 }
 
-int wps_pbc(){
+int wps_pbc(wps_t){
 	int v;
 
 	system("/home/rtl/wpa_cli -iwlan0 wps_pbc");
 	delay(3);
-	v= timer();
+	v= timer(wps_t);
 	if(v==0){
 		printf("Connnect Fail!!\n");
+
 	}else{
 		printf("Device Connecting!!\n");
 		system("/home/rtl/wpa_cli -iwlan0 save_config");
+		get_state();
+
 	}
 	return 0;
 }
 
-int wps_pin(){
+int wps_pin(wps_t){
 	int v;
 
 	system("/home/rtl/wpa_cli -iwlan0 wps_pin any > /tmp/pin.txt");
 	//delay(3);
-	v= timer();
+	v= timer(wps_t);
 	if(v==0){
 		printf("Connnect Fail!!\n");
 		system("rm /tmp/pin.txt");
+
 	}else{
 		printf("Device Connecting!!\n");
 		system("rm /tmp/pin.txt");
 		system("/home/rtl/wpa_cli -iwlan0 save_config");
+		get_state();
+
 	}
 	return 0;
 }
