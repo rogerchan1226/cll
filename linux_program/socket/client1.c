d845b55cf0185b0a76d7f2695a34ab60fc50f282
roger_chan <roger_chan@calcomp.com>
2019-06-03 15:03:19 +0800

    socket_UDP_AFUNIX

--- linux_program/socket/client1.c
+++ linux_program/socket/client1.c
@@ -6,31 +6,30 @@
 #include <stdlib.h>
 
 
+int socket_send(){
+	printf("Start sending string !!!");
+	for(int i=0; i++; i=10){
 
+	}
+}
 
-int main(){
+int main(int argc, char *argv[]){
 	int sockfd;
 	int len;
 	struct sockaddr_un address;
-	int result;
-	char ch = 'A';
+	char string_send[256] = {0};
 
-	sockfd = socket(AF_UNIX, SOCK_STREAM, 0);		//build the socket for client1
+	sockfd = socket(AF_UNIX, SOCK_STREAM, 0);		                        //build the socket for client1
 
-	address.sun_family = AF_UNIX;				    //	{
-	strcpy(address.sun_path, "server_socket");		//		name the socket to match server
-	len = sizeof(address);							//	}
-	
-	result = connect(sockfd, (struct sockaddr *)&address, len);		//connect to server socket
-
-	if(result == -1){
-		perror("oops: client1");
-		exit(0);
+	address.sun_family = AF_UNIX;				                            //	{
+	strcpy(address.sun_path, "server_socket");		                        //		name the socket to match server
+	len = sizeof(address);							                        //	}
+		
+	if(connect(sockfd, (struct sockaddr *)&address, len) < 0){              //connect to server socket
+		perror("oops: client");
+		return -1;
 	}
 
-	write(sockfd, &ch, 1);		//to write by sockfd
-	read(sockfd, &ch, 1);		//to read  by sockfd
-	printf("char from server = %c\n", ch);
-	close(sockfd);
-	exit(0);
+	socket_send();
+	return 0;
 }
