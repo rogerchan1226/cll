8c18672f343faa29b3d1890f0a2c696965269fdc
roger_chan <roger_chan@calcomp.com>
2019-06-05 14:46:49 +0800

    socket_dgram_done

--- linux_program/socket/client1.c
+++ linux_program/socket/client1.c
@@ -6,30 +6,39 @@
 #include <stdlib.h>
 
 
-int socket_send(){
-	printf("Start sending string !!!");
-	for(int i=0; i++; i=10){
+static struct sockaddr_un address;
 
+int socket_send(int sock, int address_len){
+	int i;
+	char num[5]={0};
+	printf("Start sending string !!!\n");
+
+	for(i=0; i<=10; i++){
+		sprintf(num, "%d", i);
+		if(sendto(sock, num, sizeof(num), 0,(const struct sockaddr *)&address, address_len) < 0){
+			return -1;
+			break;
+		}
+		sendto(sock, ". message get!\n", 17, 0, (const struct sockaddr *)&address, address_len);
+		sleep(1);
 	}
+	return 0;
 }
 
 int main(int argc, char *argv[]){
 	int sockfd;
 	int len;
-	struct sockaddr_un address;
 	char string_send[256] = {0};
 
-	sockfd = socket(AF_UNIX, SOCK_STREAM, 0);		                        //build the socket for client1
+	sockfd = socket(AF_UNIX, SOCK_DGRAM, 0);		                        //build the socket for client1
 
-	address.sun_family = AF_UNIX;				                            //	{
-	strcpy(address.sun_path, "server_socket");		                        //		name the socket to match server
-	len = sizeof(address);							                        //	}
-		
-	if(connect(sockfd, (struct sockaddr *)&address, len) < 0){              //connect to server socket
-		perror("oops: client");
-		return -1;
+	address.sun_family = AF_UNIX;				                            //name the socket to match server
+	strcpy(address.sun_path, "server_socket");
+
+	len = sizeof(struct sockaddr_un);
+
+	if(socket_send(sockfd, len) < 0){
+		perror("Send Error: ");
 	}
-
-	socket_send();
 	return 0;
 }
