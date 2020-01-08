e9d18513a1779939ebc8a5a35391db237c7f0d0b
roger <rogerchan1226@gmail.com>
2020-01-08 23:18:00 +0800

    [Leetcode] Update Longest_Common_Prefix.c

--- leetcode/Longest_Common_Prefix.c
+++ leetcode/Longest_Common_Prefix.c
@@ -8,11 +8,39 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 
 
 
 char * longestCommonPrefix(char ** strs, int strsSize){
+    int k = 0, l = 0;
+    char ** ptr = strs;     // set as pointer strs's initialization address
+    char *lock = NULL;
+    char result;
+
+    while(k < strsSize){    // pick up smallest strs address
+        if(lock == NULL){
+            if(strlen(strs[k]) <= strlen(strs[k+1]))
+                lock = strs[k];
+        }else{
+            if(strlen(lock) >= strlen(strs[k]))
+                lock = strs[k];
+        }
+        k++;
+    }
+    k = strlen(lock) -1;
+
+    while(l <= k){
+        do{
+            if(*strs[l] != *strs[l+1])
+                break;
+            else{
+                while()
+            }
+                
+        }while(l < strsSize);
+    }
     
     return ;
 }
@@ -22,6 +50,10 @@ char * longestCommonPrefix(char ** strs, int strsSize){
   *     You may setup any question detail by 
   *     modify "input" value.
   **/
+void freeAllMemory(){
+    
+}
+
 void main(void){
     int i,
         j=2;
@@ -42,9 +74,10 @@ void main(void){
     *input = "flight";
 
     input = temp;
-    printf("input: %s\n", input[0]);
+    printf("input: %s, %s, %s\n", input[0], input[1], input[2]);
 
     output = longestCommonPrefix(input, 3);
     
     printf("output: %s\n", output);
+    freeAllMemory();
 }
