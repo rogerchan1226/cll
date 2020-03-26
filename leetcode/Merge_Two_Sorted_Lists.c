763fcab12695d3942469cc0b1362a3cc3570465c
roger_chan <roger_chan@calcomp.com>
2020-03-26 16:49:25 +0800

    [Leetcode] Update Merge_Two_Sorted_Lists.c

--- leetcode/Merge_Two_Sorted_Lists.c
+++ leetcode/Merge_Two_Sorted_Lists.c
@@ -26,8 +26,37 @@ struct ListNode* mergeTwoLists(struct ListNode* l1, struct ListNode* l2){
                     *result;
     
     result = l1;
+    buffer = l2;
 
-    
+    return result;
+}
+
+void freeLinkList(struct ListNode* linklist){
+    struct ListNode* buffer;
+
+    while(1){
+        buffer = linklist;
+        linklist = linklist->next;
+        free(buffer);
+        if(linklist){
+            continue;
+        }
+        break;
+    }
+}
+
+void showResult(struct ListNode* output){
+
+    while(1){
+        printf("%d", output->val);
+        if(output->next){
+            output = output->next;
+            printf("->");
+            continue;
+        }
+        printf("\n");
+        break;
+    }
 }
 
 void main(){
@@ -40,44 +69,55 @@ void main(){
  *      Input:  1->2->4, 1->3->4
  *      Output: 1->1->2->3->4->4
  */
-    struct ListNode *l1,        /* l1 input struct */
-                    *l2,        /* l2 input struct */
+    int cnt,
+        nums;
+    /* You can modify input value from below int array. */
+    int input1[] ={1,2,4},
+        input2[] ={1,3,4}; 
+    struct ListNode *l1,
+                    *l1o,       /* List 1 origin pointer */       
+                    *l2,
+                    *l2o,       /* List 2 origin pointer */
                     *buffer,
                     *output;
 
 //========== l1 linklist setting ===================================
     l1 = (struct ListNode*)calloc(1, sizeof(struct ListNode));
+    l1o = l1;
+    nums = sizeof(input1)/sizeof(int);
 
-    l1->val = 1;
-    buffer = (struct ListNode*)calloc(1, sizeof(struct ListNode));
-    l1->next = buffer;
-    l1 = buffer;
+    for(cnt=0; cnt<nums; cnt++){
+        l1->val=input1[cnt];
+        if((cnt+1) == nums){
+            l1->next = NULL;
+            break;
+        }
+        buffer = (struct ListNode*)calloc(1, sizeof(struct ListNode));
+        l1->next = buffer;
+        l1 = l1->next;
+    }
 
-    buffer = l1->next;
-    buffer = (struct ListNode*)calloc(1, sizeof(struct ListNode));
-    l1->val = 2;
-    l1 = buffer;
-
-    buffer = l1->next;
-    buffer = (struct ListNode*)calloc(1, sizeof(struct ListNode));
-    l1->val = 4;
-    
 //========== l2 linklist setting ===================================
     l2 = (struct ListNode*)calloc(1, sizeof(struct ListNode));
+    l2o = l2;
+    nums = sizeof(input2)/sizeof(int);
 
-    l2->val = 1;
-    buffer = (struct ListNode*)calloc(1, sizeof(struct ListNode));
-    l2->next = buffer;
-    l2 = buffer;
+    for(cnt=0; cnt<nums; cnt++){
+        l2->val=input2[cnt];
+        if((cnt+1) == nums){
+            l2->next = NULL;
+            break;
+        }
+        buffer = (struct ListNode*)calloc(1, sizeof(struct ListNode));
+        l2->next = buffer;
+        l2 = l2->next;
+    }
 
-    buffer = l2->next;
-    buffer = (struct ListNode*)calloc(1, sizeof(struct ListNode));
-    l2->val = 3;
-    l2 = buffer;
+    output = mergeTwoLists(l1o, l2o);
+    
+    showResult(output);     /* Show answer */
 
-    buffer = l2->next;
-    buffer = (struct ListNode*)calloc(1, sizeof(struct ListNode));
-    l2->val = 4;
-
-    output = mergeTwoLists(l1, l2);
+    freeLinkList(l1o);
+    freeLinkList(l2o);
+    freeLinkList(output);
 }
